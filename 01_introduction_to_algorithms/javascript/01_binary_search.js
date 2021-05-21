4e63566a98710676f10d0c42086a067ad7130cb1
stepperweb <erick1129@naver.com>
2021-05-21 22:39:41 +0900

    check

--- 01_introduction_to_algorithms/javascript/01_binary_search.js
+++ 01_introduction_to_algorithms/javascript/01_binary_search.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
 function binary_search(list, item) {
   let low = 0;
   let high = list.length - 1;
-  
+
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
     let guess = list[mid];
-    if (guess === item) {
+    if (guess === item) {
       return mid;
     }
     if (guess > item) {
@@ -16,7 +16,7 @@ function binary_search(list, item) {
       low = mid + 1;
     }
   }
-  
+
   return null;
 }
 
@@ -24,4 +24,3 @@ const my_list = [1, 3, 5, 7, 9];
 
 console.log(binary_search(my_list, 3)); // 1
 console.log(binary_search(my_list, -1)); // null
- 
